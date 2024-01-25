0a3bb704d1a840fbc91cb87a628dcefec54d8044
yaarrraa <viktorova.yaroslava26@gmail.com>
2024-01-25 12:44:48 +0200

    third homework

--- lesson_15/hw_3.py
+++ lesson_15/hw_3.py
@@ -1,21 +1,21 @@
 value_1=input("Please enter value_1 = ")
 value_2=input("Please enter value_2 = ")
-action=input ("Please select th action: '+', '-', '*', '/': ")
+action=input ("Please select the action: '+', '-', '*', '/': ")
 
 if action == "+":
-    result_sum = int(value_1)+int(value_2)
+    result_sum = float(value_1)+float(value_2)
     print(f"value_1 + value_2 = {result_sum}")
 elif action == "-":
-    result_minus = int(value_1)-int(value_2)
+    result_minus = float(value_1)-float(value_2)
     print(f"value_1 - value_2 = {result_minus}")
 elif action == "*":
-    result_mult = int(value_1)*int(value_2)
+    result_mult = float(value_1)*float(value_2)
     print(f"value_1 * value_2 = {result_mult}")
 elif action == "/":
-    if int(value_2)==0:
+    if float(value_2) == 0.0:
         print("Your value_2 cannot be 0")
-    elif int(value_2)!=0:
-        result_div = int(value_1)/int(value_2)
+    elif float(value_2) != 0.0:
+        result_div = float(value_1)/float(value_2)
         print(f"value_1 / value_2 = {result_div}")
 
 
