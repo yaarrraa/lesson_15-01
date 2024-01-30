68ba8f089402abd71e5f282c1736ff2aab65709c
yaarrraa <viktorova.yaroslava26@gmail.com>
2024-01-30 12:34:50 +0200

    fifth homework

---
+++ lesson_15/hw_5.py
@@ -0,0 +1,13 @@
+my_string = input("Please enter: ")
+condition_1 = my_string.isidentifier()
+
+if condition_1:
+    if my_string == "_":
+        print("True")
+    else:
+        condition_2 = my_string.islower()
+        print(condition_2)
+else:
+    print(condition_1)
+
+
