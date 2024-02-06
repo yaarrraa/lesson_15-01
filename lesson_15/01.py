36958c9950ef964ff50eb1ac188e40b487821dad
yaarrraa <viktorova.yaroslava26@gmail.com>
2024-02-06 12:54:06 +0200

    first homework

--- lesson_15/01.py
+++ lesson_15/01.py
@@ -1,10 +1,16 @@
-number = input("Please enter your number: ")
-print("Your number is " + number)
-numeral_1 = int(number)//1000
-print(numeral_1)
-numeral_2 = (int(number)%1000)//100
-print(numeral_2)
-numeral_3 = (int(number)%100)//10
-print(numeral_3)
-numeral_4 = int(number)%10
-print(numeral_4)
+#number = input("Please enter your number: ")
+#print("Your number is " + number)
+##print(numeral_1)
+#numeral_2 = (int(number)%1000)//100
+#print(numeral_2)
+#numeral_3 = (int(number)%100)//10
+#print(numeral_3)
+#numeral_4 = int(number)%10
+#print(numeral_4)
+
+value = "1" + "2" * 3
+
+print(value)
+
+
+
