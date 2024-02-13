f7e7e519ba08d1addfcf655002e6e009d0025a31
yaarrraa <viktorova.yaroslava26@gmail.com>
2024-02-13 15:16:54 +0200

    eighth homework

---
+++ lesson_15/hw_8_2.py
@@ -0,0 +1,13 @@
+def find_unique_value(some_list):
+    unique_number = 0
+    for number in some_list:
+        if some_list.count(number) == 1:
+            unique_number = number
+    return unique_number
+
+
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+print("ОК")
+
